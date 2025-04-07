d2782d0122d35d42e1cbcd5b4f73bb96f1326fe6
mattjfrancis <99334443+mattjfrancis@users.noreply.github.com>
2025-04-07 15:20:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -155,4 +156,3 @@ with tab2:
         col1.metric("Total Energy", f"{df['Grid Energy (kWh)'].sum():.2f} kWh")
         col2.metric("Carbon Offset", f"{df['CO2 Offset (kg)'].sum():.2f} kg")
         col3.metric("Revenue", f"£{df['Reg Revenue (£)'].sum():.2f}")
- 
